75eb895b24202984478c4d94224b490567275a89
Vitor Moreira <vitor.sousa.moreira@hotmail.com>
2020-09-04 01:56:32 +0000

    fixing dotnet version and adding login page

---
+++ purchase_list_csharp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using purchase_list_bg.Data;
+using purchase_list_bg.Models;
+using purchase_list_csharp.Models;
+
+namespace purchase_list_csharp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly UserContext _context;
+
+        public HomeController(UserContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ purchase_list_csharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace purchase_list_csharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
